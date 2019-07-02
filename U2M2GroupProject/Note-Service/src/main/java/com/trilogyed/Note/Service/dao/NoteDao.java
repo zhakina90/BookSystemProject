c1e1f1f0649e5f8ec3b45866aad26c693d57ae20
Bria J'Nae <walker.briaj@gmail.com>
2019-07-02 15:58:31 -0500

    adds DAO, JDBC, and tests

---
+++ U2M2GroupProject/Note-Service/src/main/java/com/trilogyed/Note/Service/dao/NoteDao.java
@@ -0,0 +1,22 @@
+package com.trilogyed.Note.Service.dao;
+
+import com.trilogyed.Note.Service.model.Note;
+
+
+import java.util.List;
+
+
+public interface NoteDao {
+
+    Note createNote(Note note);
+
+    Note getNote(int id);
+
+   List<Note> getNotesByBook(int book_id);
+
+    List<Note> getAllNotes();
+
+    void updateNote(Note note);
+
+    void deleteNote(int note_id);
+}
