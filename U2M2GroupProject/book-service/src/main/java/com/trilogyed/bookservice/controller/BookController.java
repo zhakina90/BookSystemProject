48c9449975798cdd610df604a061df3efaf43cba
zhakina90 <zhakina90@gmail.com>
2019-07-09 21:48:00 -0500

    BookService Mock, CLeaned Up codes, Removed NoteListner

--- U2M2GroupProject/book-service/src/main/java/com/trilogyed/bookservice/controller/BookController.java
+++ U2M2GroupProject/book-service/src/main/java/com/trilogyed/bookservice/controller/BookController.java
@@ -1,23 +1,14 @@
 package com.trilogyed.bookservice.controller;
 
-import com.trilogyed.bookservice.dao.BookDao;
-import com.trilogyed.bookservice.exception.NotFoundException;
-import com.trilogyed.bookservice.model.Book;
 import com.trilogyed.bookservice.service.BookService;
 import com.trilogyed.bookservice.util.feign.NoteServiceClient;
-import com.trilogyed.bookservice.util.messages.NoteListEntry;
 import com.trilogyed.bookservice.viewModel.BookViewModel;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-
-import static com.trilogyed.bookservice.service.BookService.EXCHANGE;
-import static com.trilogyed.bookservice.service.BookService.ROUTING_KEY;
 
 @RestController
 public class BookController {
@@ -26,10 +17,6 @@ public class BookController {
 
     @Autowired
     private final NoteServiceClient client;
-//
-//    @Autowired
-//    RabbitTemplate rabbitTemplate;
-
 
     BookController(NoteServiceClient client){
 
@@ -38,22 +25,11 @@ public class BookController {
     }
 
 
-//    @RequestMapping(value = "/note", method = RequestMethod.GET)
-//    public String note(){
-//        return client.getNote(6);
-//    }
-
-
     @RequestMapping(value = "/books", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public BookViewModel createBook(@RequestBody @Valid BookViewModel bookViewModel){
 
         bookService.addBook(bookViewModel);
-//        NoteListEntry msg = new NoteListEntry();
-//        msg.setBookId(bookViewModel.getBookId());
-//        msg.setNote(bookViewModel.getNote());
-//        rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, msg);
-
         return bookViewModel;
 
     }
@@ -77,8 +53,7 @@ public class BookController {
     @RequestMapping(value = "/books/{id}", method = RequestMethod.PUT)
     @ResponseStatus(value = HttpStatus.OK)
     public BookViewModel updateBook(@PathVariable("id") int bookId, @RequestBody BookViewModel bookViewModel){
-//        if(bookViewModel.getBookId() == 0)
-//            bookViewModel.setBookId(bookId);
+
         if(bookId != bookViewModel.getBookId()){
             throw new IllegalArgumentException("Book ID: " + bookId + " must match the ID in the Book Object.");
         }
